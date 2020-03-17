b25fe2a29099918997d7b0a7a43d70a9e02513d7
jonson <jonson@dajodi.com>
2020-03-17 08:25:12 -0400

    Log exceptions
    On both handled and unhandled exceptions, log them using the python
    logging framework.

--- flapison/__init__.py
+++ flapison/__init__.py
@@ -4,6 +4,11 @@ from flapison.api import Api
 from flapison.resource import ResourceList, ResourceDetail, ResourceRelationship
 from flapison.exceptions import JsonApiException
 
+import logging
+from logging import NullHandler
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
 __all__ = [
     "Api",
     "ResourceList",
